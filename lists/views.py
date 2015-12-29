029127e423713443a01d10d80bd174ee854fd103
zvovov <zvovov@gmail.com>
2015-12-29 15:17:59 +0530

    new URL for adding lists, new view

--- lists/views.py
+++ lists/views.py
@@ -4,13 +4,12 @@ from .models import Item
 
 # Create your views here.
 def home_page(request):
-    if request.method=='POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world')
-        
-    items = Item.objects.all()
     return render(request, 'lists/home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'lists/list.html', {'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world')
--- superlists/urls.py
+++ superlists/urls.py
@@ -20,5 +20,6 @@ from lists import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^lists/the-only-list-in-the-world$', views.view_list, name='view_list'),
+    url(r'^lists/new$', views.new_list, name='new_list'),
     url(r'^$', views.home_page, name='home'),
 ]
